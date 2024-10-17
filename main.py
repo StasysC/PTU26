3a28999b62be6f5494ff9a80d0fc17009c83b978
Stasys Civilis <scivitas@gmail.com>
2024-10-17 10:38:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+pagrindinis = Tk()
+uzrasas = Label(pagrindinis, width=40, height=10, text="Sveikas, pasauli!")
+pagrindinis.title("Mano programa")
+pagrindinis.iconbitmap(r'html.ico')
+uzrasas.pack()
+
+pagrindinis.mainloop()
